91b2503a31bd4a4702a1ac35df8bfcc996b48a0c
Oliver Hoffmann <oliverhoffmann32@googlemail.com>
2009-12-26 18:28:07 +0000

    added primitive command-line parsing

--- src/pleocmd/itfc/cli/CommandLine.java
+++ src/pleocmd/itfc/cli/CommandLine.java
@@ -1,5 +1,12 @@
 package pleocmd.itfc.cli;
 
+import java.io.File;
+import java.io.IOException;
+
+import pleocmd.Log;
+import pleocmd.exc.PipeException;
+import pleocmd.pipe.Pipe;
+
 /**
  * @author oliver
  */
@@ -7,13 +14,57 @@ public final class CommandLine {
 
 	private static CommandLine commandLine;
 
+	private final Pipe pipe;
+
+	private CommandLine() {
+		commandLine = this;
+		pipe = new Pipe();
+	}
+
 	public static CommandLine the() {
-		if (commandLine == null) commandLine = new CommandLine();
+		if (commandLine == null) new CommandLine();
 		return commandLine;
 	}
 
 	public void parse(final String[] args) {
-		// TODO
+		try {
+			// TODO already have some detailed output here
+			Log.setLogDetailed(false);
+			for (final String arg : args) {
+				final File file = new File(arg);
+				if (file.isFile())
+					if (file.getName().endsWith(".pca"))
+						executePipe(file);
+					else
+						throw new RuntimeException(String.format(
+								"Cannot recognize filetype of '%s'", file));
+				else if ("-d".equals(arg) || "--detailed".equals(arg))
+					Log.setLogDetailed(true);
+				else
+					throw new RuntimeException(String.format(
+							"Cannot recognize argument '%s'", file));
+			}
+		} catch (final Throwable throwable) { // CS_IGNORE
+			// we need to print the Exception additionally to
+			// logging it because logging itself may have caused
+			// the exception or it just is not yet initialized
+			throwable.printStackTrace(); // CS_IGNORE
+			Log.error(throwable, "Failed to parse command line arguments");
+		}
+	}
+
+	public void executePipe(final File pcaFile) {
+		try {
+			pipe.readFromFile(pcaFile);
+			pipe.configure();
+			pipe.pipeAllData();
+		} catch (final IOException e) {
+			Log.error(e);
+		} catch (final PipeException e) {
+			Log.error(e);
+		} catch (final InterruptedException e) {
+			Log.error(e);
+		}
 	}
 
 }
