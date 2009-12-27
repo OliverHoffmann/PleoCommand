cee27a76ec998f0fc06b44a6baf99f5b5628226c
Oliver Hoffmann <oliverhoffmann32@googlemail.com>
2009-12-27 13:13:52 +0000

    added JUnit test

---
+++ test/pleocmd/StandardInputTest.java
@@ -0,0 +1,56 @@
+package pleocmd;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.Random;
+
+import org.junit.Test;
+
+import pleocmd.itfc.gui.MainFrame;
+
+public class StandardInputTest {
+
+	private static final int BUF_SIZE = 16 * 1024;
+	private static final long RAND_SEED = 292175611265159452L;
+
+	@Test(timeout = 60000)
+	public void testPutAndRead() throws IOException {
+		// prepare
+		final byte buf0[] = new byte[BUF_SIZE];
+		final byte buf1[] = new byte[BUF_SIZE];
+		new Random(RAND_SEED).nextBytes(buf0);
+
+		// ensure we are in GUI mode
+		MainFrame.the();
+		// no need for detailed output
+		Log.setLogDetailed(false);
+
+		// execute twice
+		for (int i = 0; i < 2; ++i) {
+			System.out.println("Starting test loop for StandardInput");
+			StandardInput.the().put(buf0);
+			System.out.println("Put bytes into StandardInput");
+			assertEquals(StandardInput.the().available(), buf0.length);
+			System.out.println("Checked available bytes");
+			StandardInput.the().read(buf1);
+			System.out.println("Read bytes from StandardInput");
+			assertArrayEquals(buf0, buf1);
+
+			StandardInput.the().close();
+			System.out.println("Closed StandardInput");
+
+			// now available() should no longer block ...
+			assertEquals(StandardInput.the().available(), 0);
+
+			// ... and read should fail
+			assertEquals(StandardInput.the().read(), -1);
+
+			StandardInput.the().resetCache();
+			System.out.println("Reset StandardInput");
+		}
+		System.out.println("Done testing StandardInput");
+	}
+
+}
