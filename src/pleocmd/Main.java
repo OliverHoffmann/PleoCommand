664689f2d85b1c93d198648dd7326123a7b269af
Oliver Hoffmann <oliverhoffmann32@googlemail.com>
2010-01-18 21:37:31 +0000

    updated TODO list

--- src/pleocmd/Main.java
+++ src/pleocmd/Main.java
@@ -3,6 +3,7 @@ package pleocmd;
 import java.awt.EventQueue;
 
 import pleocmd.itfc.cli.CommandLine;
+import pleocmd.itfc.gui.DataSequenceEditorFrame;
 import pleocmd.itfc.gui.MainFrame;
 
 /**
@@ -15,6 +16,10 @@ import pleocmd.itfc.gui.MainFrame;
  * <br>
  * more Log.debug<br>
  * display power status<br>
+ * disable "OK" and "Apply" if bad input in PipePart configuration<br>
+ * online help<br>
+ * combine History and {@link DataSequenceEditorFrame}<br>
+ * make dialogs non-modal?<br>
  */
 
 public final class Main {
