aa69bdb18644e908a3541c8371dc42575e00b689
Oliver Hoffmann <oliverhoffmann32@googlemail.com>
2010-04-24 11:06:39 +0000

    added additional downloads to build.bat and build.sh made build.bat more robust

--- installation/build.bat
+++ installation/build.bat
@@ -1,14 +1,20 @@
 @ECHO OFF
 
-SET part=0
-SET count=8
 SETLOCAL enabledelayedexpansion
-SET "failed=0"
+
+SET part=0
+SET count=17
+
+SET myexit=EXIT /B
+if X%1 == Xfrom-ant (
+	SHIFT
+	SET myexit=EXIT
+)
 
 GOTO MAIN
 
 :CHECK
-	SET "checkres=0"
+	SET checkres=0
 	IF NOT EXIST %1 (
 		ECHO %1 does not exist
 		GOTO:EOF
@@ -18,13 +24,14 @@ GOTO MAIN
 		ECHO Unexpected filesize of %1: Got %size% but should be %2
 		GOTO:EOF
 	)
-	FOR /F "tokens=1" %%A IN ('md5sum %1') DO SET sum=%%A
-	SET sum=%sum:~1%
+	FOR /F "tokens=1" %%A IN ('md5sum -b %1') DO SET sum=%%A
+	IF X%sum:~0,1% == X\ SET sum=%sum:~1%
+	IF X%sum:~0,1% == X* SET sum=%sum:~1%
 	IF X"%sum%" NEQ X%3 (
 		ECHO Unexpected md5-sum of %1: Got "%sum%" but should be %3
 		GOTO:EOF
 	)
-	SET "checkres=1"
+	SET checkres=1
 GOTO:EOF
 
 :DL
@@ -42,9 +49,8 @@ GOTO:EOF
 		wget %url% --progress=dot:mega --output-document %fn% %5 %6 %7 %8 %9
 		CALL :CHECK %fn% %fs% %fh%
 		IF !checkres! == 0 (
-			SET "failed=1"
 			ECHO "!!! Downloading failed !!!"
-			GOTO:EOF
+			%myexit% 1
 		)
 	)
 	ECHO ======================================================================
@@ -60,36 +66,55 @@ GOTO:EOF
 		DEL "development\eclipse-java-galileo-SR2-win32.zip"
 		DEL "development\pawn-3.3.4127.package"
 		DEL "development\PleoDevelopmentKit.zip"
-		EXIT 0
+		DEL "development\apache-ant-1.8.0-bin.zip"
+		DEL "development\coreutils-5.3.0.exe"
+		DEL "development\wget-1.11.4-1-setup.exe"
+		DEL "development\MinGW-5.1.6.exe"
+		DEL "development\Setup-Subversion-1.6.6.msi"
+		DEL "development\xsltproc\iconv-1.9.2.win32.zip"
+		DEL "development\xsltproc\libxml2-2.7.6.win32.zip"
+		DEL "development\xsltproc\libxmlsec-1.2.13.win32.zip"
+		DEL "development\xsltproc\libxslt-1.1.26.win32.zip"
+		%myexit% 0
 	)
 	REM runtime
 	CALL :DL "http://bci2000.org/downloads/bin/BCI2000Setup_091110.exe" ^
 		"runtime\BCI2000Setup_091110.exe" 35760134 "54198c14540d012c9b403c749bc725cb" ^
 		--user "ascheck" --password "eibah7cohB"
-	IF %failed% == 1 EXIT 1
 	CALL :DL "http://javadl.sun.com/webapps/download/AutoDL?BundleId=39485" ^
 		"runtime\jre-6u20-linux-i586.bin" 21079390  "a6d7381cbca6ffcb1670f5e7eea1d41b"
-	IF %failed% == 1 EXIT 1
 	CALL :DL "http://javadl.sun.com/webapps/download/AutoDL?BundleId=39494" ^
 		"runtime\jre-6u20-windows-i586-s.exe" 16529184 "71fdde020a4920f55c96e1121a1dbd4a"
-	IF %failed% == 1 EXIT 1
 	CALL :DL "http://www.dogsbodynet.com/myskit/downloads/MySkit-v1.3-Installer.exe" ^
 		"runtime\MySkit-v1.3-Installer.exe" 3666219 "880a163f157d377ea8fb7bb203e3dbc0"
-	IF %failed% == 1 EXIT 1
 	CALL :DL "http://www.pleoworld.com/downloads/PleoSDSoftware_1.1.zip" ^
 		"runtime\PleoSDSoftware_1.1.zip" 2273358 "3812d55f0d9d5c88066b9ee16793523f"
-	IF %failed% == 1 EXIT 1
 
 	REM development
+	CALL :DL "http://mirror.netcologne.de/apache.org/ant/binaries/apache-ant-1.8.0-bin.zip" ^
+		"development/apache-ant-1.8.0-bin.zip" 12088734 "c9eaa7b72e728a40ca748ff8e1fc6869"
+	CALL :DL "http://ignum.dl.sourceforge.net/project/gnuwin32/coreutils/5.3.0/coreutils-5.3.0.exe" ^
+		"development/coreutils-5.3.0.exe" 6439882 "5a3e9d30b906dadf54de0635522fd62c"
+	CALL :DL "http://surfnet.dl.sourceforge.net/project/gnuwin32/wget/1.11.4-1/wget-1.11.4-1-setup.exe" ^
+		"development/wget-1.11.4-1-setup.exe" 3012464 "b4679ac6f7757b35435ec711c6c8d912"
+	CALL :DL "http://downloads.sourceforge.net/project/mingw/Automated%%%%20MinGW%%%%20Installer/MinGW%%%%205.1.6/MinGW-5.1.6.exe" ^
+		"development/MinGW-5.1.6.exe" 158842 "9cf4ab0b4c9f858d32f5d5c89009c4dc"
+	CALL :DL "http://subversion.tigris.org/files/documents/15/46906/Setup-Subversion-1.6.6.msi" ^
+		"development/Setup-Subversion-1.6.6.msi" 4834672 "379fbeb45067f98ebd830affd4afdcc1"
+	CALL :DL "ftp://ftp.zlatkovic.com/libxml/iconv-1.9.2.win32.zip" ^
+		"development/xsltproc/iconv-1.9.2.win32.zip" 1320616 "da5f3164bbd21e7830faef7e5a7ea5e6"
+	CALL :DL "ftp://ftp.zlatkovic.com/libxml/libxml2-2.7.6.win32.zip" ^
+		"development/xsltproc/libxml2-2.7.6.win32.zip" 2731818 "d7b1f133b286751cdf5a50f39dbb6ddf"
+	CALL :DL "ftp://ftp.zlatkovic.com/libxml/libxmlsec-1.2.13.win32.zip" ^
+		"development/xsltproc/libxmlsec-1.2.13.win32.zip" 954228 "c6bafb19bb02eb733f7f3f672e736766"
+	CALL :DL "ftp://ftp.zlatkovic.com/libxml/libxslt-1.1.26.win32.zip" ^
+		"development/xsltproc/libxslt-1.1.26.win32.zip" 408001 "36fb352ea5b6309305476bcb8cabc31e"
 	CALL :DL "http://mirror.selfnet.de/eclipse/technology/epp/downloads/release/galileo/SR2/eclipse-java-galileo-SR2-win32.zip" ^
 		"development\eclipse-java-galileo-SR2-win32.zip" 97290250 "9e3048a9f26386130334134f4062232a"
-	IF %failed% == 1 EXIT 1
 	CALL :DL "http://www.compuphase.com/pawn/pawn-3.3.4127.package" ^
 		"development\pawn-3.3.4127.package" 3523216 "0b429bf95ed11762cb5840906b622a8a"
-	IF %failed% == 1 EXIT 1
 	CALL :DL "http://www.pleoworld.com/downloads/PleoDevelopmentKit.zip" ^
 		"development\PleoDevelopmentKit.zip" 53194900 "cfc20448351b6b13c3908f36ca40f5c5"
-	IF %failed% == 1 EXIT 1
 
 	ECHO "Finished downloading and md5-checking of all files."
-	EXIT 0
+	%myexit% 0
--- installation/build.sh
+++ installation/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 part=0
-count=8
+count=17
 
 function check() { # filename size hash
 	[ -e "$1" ] || return 1
@@ -10,7 +10,7 @@ function check() { # filename size hash
 		echo "Unexpected filesize of $1: Got $size but should be $2"
 		return 1
 	fi
-	sum=`md5sum "$1" | cut -d " " -f 1`
+	sum=`md5sum -b "$1" | cut -d " " -f 1`
 	if [ "$sum" != "$3" ]; then
 		echo "Unexpected md5-sum of $1: Got $sum but should be $3"
 		return 1
@@ -51,8 +51,17 @@ if [ "$1" == "clean" ]; then
 	rm -vf "development/eclipse-java-galileo-SR2-win32.zip"
 	rm -vf "development/pawn-3.3.4127.package"
 	rm -vf "development/PleoDevelopmentKit.zip"
+	rm -vf "development/apache-ant-1.8.0-bin.zip"
+	rm -vf "development/coreutils-5.3.0.exe"
+	rm -vf "development/wget-1.11.4-1-setup.exe"
+	rm -vf "development/MinGW-5.1.6.exe"
+	rm -vf "development/Setup-Subversion-1.6.6.msi"
+	rm -vf "development/xsltproc/iconv-1.9.2.win32.zip"
+	rm -vf "development/xsltproc/libxml2-2.7.6.win32.zip"
+	rm -vf "development/xsltproc/libxmlsec-1.2.13.win32.zip"
+	rm -vf "development/xsltproc/libxslt-1.1.26.win32.zip"
 else
-	#runtime
+	# runtime
 	dl "http://bci2000.org/downloads/bin/BCI2000Setup_091110.exe" \
 		"runtime/BCI2000Setup_091110.exe" 35760134 "54198c14540d012c9b403c749bc725cb" +x \
 		--user "ascheck" --password "eibah7cohB"
@@ -66,6 +75,24 @@ else
 		"runtime/PleoSDSoftware_1.1.zip" 2273358 "3812d55f0d9d5c88066b9ee16793523f"
 
 	# development
+	dl "http://mirror.netcologne.de/apache.org/ant/binaries/apache-ant-1.8.0-bin.zip" \
+		"development/apache-ant-1.8.0-bin.zip" 12088734 "c9eaa7b72e728a40ca748ff8e1fc6869"
+	dl "http://ignum.dl.sourceforge.net/project/gnuwin32/coreutils/5.3.0/coreutils-5.3.0.exe" \
+		"development/coreutils-5.3.0.exe" 6439882 "5a3e9d30b906dadf54de0635522fd62c" +x
+	dl "http://surfnet.dl.sourceforge.net/project/gnuwin32/wget/1.11.4-1/wget-1.11.4-1-setup.exe" \
+		"development/wget-1.11.4-1-setup.exe" 3012464 "b4679ac6f7757b35435ec711c6c8d912" +x
+	dl "http://downloads.sourceforge.net/project/mingw/Automated%20MinGW%20Installer/MinGW%205.1.6/MinGW-5.1.6.exe" \
+		"development/MinGW-5.1.6.exe" 158842 "9cf4ab0b4c9f858d32f5d5c89009c4dc" +x
+	dl "http://subversion.tigris.org/files/documents/15/46906/Setup-Subversion-1.6.6.msi" \
+		"development/Setup-Subversion-1.6.6.msi" 4834672 "379fbeb45067f98ebd830affd4afdcc1" +x
+	dl "ftp://ftp.zlatkovic.com/libxml/iconv-1.9.2.win32.zip" \
+		"development/xsltproc/iconv-1.9.2.win32.zip" 1320616 "da5f3164bbd21e7830faef7e5a7ea5e6"
+	dl "ftp://ftp.zlatkovic.com/libxml/libxml2-2.7.6.win32.zip" \
+		"development/xsltproc/libxml2-2.7.6.win32.zip" 2731818 "d7b1f133b286751cdf5a50f39dbb6ddf"
+	dl "ftp://ftp.zlatkovic.com/libxml/libxmlsec-1.2.13.win32.zip" \
+		"development/xsltproc/libxmlsec-1.2.13.win32.zip" 954228 "c6bafb19bb02eb733f7f3f672e736766"
+	dl "ftp://ftp.zlatkovic.com/libxml/libxslt-1.1.26.win32.zip" \
+		"development/xsltproc/libxslt-1.1.26.win32.zip" 408001 "36fb352ea5b6309305476bcb8cabc31e"
 	dl "http://mirror.selfnet.de/eclipse/technology/epp/downloads/release/galileo/SR2/eclipse-java-galileo-SR2-win32.zip" \
 		"development/eclipse-java-galileo-SR2-win32.zip" 97290250 "9e3048a9f26386130334134f4062232a"
 	dl "http://www.compuphase.com/pawn/pawn-3.3.4127.package" \
