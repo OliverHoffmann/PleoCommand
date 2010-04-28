416be1af2ae0abdba3f11e4d849aa7a8c6373ea2
Oliver Hoffmann <oliverhoffmann32@googlemail.com>
2010-04-28 13:06:53 +0000

    fix: missed help files and icons during building fix: only process html files in bin and dist, keep src files as they are fix: path to xsl files was wrong

--- src/tidy-all-html.sh
+++ src/tidy-all-html.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-find "$1" -type f -name "*.html" -print0 | xargs -0rIX bash -c "
-	echo \"Processing 'X'\"
-	tidy -b -c -i -m -q -u -w 120 -ashtml -utf8 'X'
-"
+function tidyDir() {
+	find "$1" -type f -name "*.html" -print0 | xargs -0rIX bash -c "
+		echo \"Processing 'X'\"
+		tidy -b -c -i -m -q -u -w 120 -ashtml -utf8 'X'
+	"
+}
+
+while [ "$1" ]; do
+	tidyDir "$1"
+	shift
+done
 
 echo "Done"
