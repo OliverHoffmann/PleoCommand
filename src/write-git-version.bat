acc537bb2fc78cbf25ecdae8bea3deb874c71b92
Oliver Hoffmann <oliverhoffmann32@googlemail.com>
2010-09-24 15:35:18 +0200

    fix parsing of empty git output

--- src/write-git-version.bat
+++ src/write-git-version.bat
@@ -1,11 +1,15 @@
 @ECHO OFF
 
-FOR /f "delims=" %a IN ('git log -n 1 --format="%h"') DO @SET rev=%a 
+FOR /F "tokens=1" %%A IN ('git log -n 1 --format="%h"') DO (
+	SET rev=%%A
+)
 IF %rev%X NEQ X (
 	sed "s:String VERSION_GITREV.*$:String VERSION_GITREV = ""%rev%"";:" -i "%1"
 )
 
-FOR /f "delims=" %a IN ('git log -n 1 --format="%ct"') DO @SET tstamp=%a 
+FOR /F "tokens=1" %%A IN ('git log -n 1 --format="%ct"') DO (
+	SET tstamp=%%A
+)
 IF %tstamp%X NEQ X (
 	sed "s:long VERSION_DATE.*$:long VERSION_DATE = %tstamp%;:" -i "%1"
 )
